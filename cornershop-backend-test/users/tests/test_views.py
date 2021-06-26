814623895b559f91cee7d4dbc4758bed00dd3cfd
Andres vasquez <you@example.com>
2021-06-26 16:08:36 -0500

    add user views test

--- cornershop-backend-test/users/tests/test_views.py
+++ cornershop-backend-test/users/tests/test_views.py
@@ -20,6 +20,9 @@ class AuthViewTest(TestCase):
             email="admin@admintest.com",
             password="pbkdf2_sha256$180000$wBXCxFVhGmdV$F4Wh75P4ZM7uS7N1Y3Yhzzlq19JqUemroty98sJaxfQ=",
             phone_number="+1300000000",
+            is_manager=True,
+            is_staff=True,
+            is_superuser=True
         )
 
     def test_auth_credentials_invalid(self):
@@ -44,7 +47,6 @@ class AuthViewTest(TestCase):
 
     def test_refresh_token_invalid(self):
         """ validate refresh token using headers invalid """
-
         response = factory.post(
             reverse("users_login"),
             data={"email": "admin@admintest.com", "password": "Admin123#"},
@@ -79,7 +81,8 @@ class AuthViewTest(TestCase):
 
     def test_get_me_invalid(self):
         """ validate get me service using token invalid """
-        response = factory.get(reverse("users_get_me"), content_type="application/json")
+        response = factory.get(reverse("users_get_me"),
+                               content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_get_me_valid(self):
@@ -91,5 +94,107 @@ class AuthViewTest(TestCase):
         )
         data = response_auth.data
         factory.defaults["HTTP_AUTHORIZATION"] = "Bearer " + data["access"]
-        response = factory.get(reverse("users_get_me"), content_type="application/json")
+        response = factory.get(reverse("users_get_me"),
+                               content_type="application/json")
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class UserViewSetTest(TestCase):
+    """ Test UserViewSet """
+
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        User.objects.create(
+            username="Admin",
+            first_name="admin",
+            last_name="admin",
+            email="admin@admintest.com",
+            password="pbkdf2_sha256$180000$wBXCxFVhGmdV$F4Wh75P4ZM7uS7N1Y3Yhzzlq19JqUemroty98sJaxfQ=",
+            phone_number="+1300000000",
+            is_manager=True,
+            is_staff=True,
+            is_superuser=True
+        )
+
+    def setUp(self):
+        """ Initial setup """
+        response = factory.post(
+            reverse("users_login"),
+            data={"email": "admin@admintest.com", "password": "Admin123#"},
+            content_type="application/json",
+        )
+        self.token = response.data["access"]
+        self.url = "/api/v1/users/"
+
+    def test_list_user(self):
+        """ validate get List Users """
+        factory.defaults["HTTP_AUTHORIZATION"] = "Bearer " + self.token
+        response = factory.get(
+            self.url,
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_user(self):
+        """ validate create Users """
+        factory.defaults["HTTP_AUTHORIZATION"] = "Bearer " + self.token
+        response = factory.post(
+            self.url,
+            data={
+                "username": "andresvz91",
+                "first_name": "andres",
+                "last_name": "vasquez",
+                "email": "andresvz91@gmail.com",
+                "phone_number": "+57 3000000031",
+                "is_manager": True,
+                "is_superuser": True,
+                "is_staff": True
+            },
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_edit_put_user(self):
+        """ validate edit put Users """
+        factory.defaults["HTTP_AUTHORIZATION"] = "Bearer " + self.token
+        response = factory.put(
+            self.url + '4/',
+            data={
+                "username": "andresvz91s",
+                "first_name": "andress",
+                "last_name": "vasquezz",
+                "email": "admin@admintest.com",
+                "phone_number": "+57 3000000032",
+                "is_manager": True,
+                "is_superuser": True,
+                "is_staff": True
+            },
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_edit_patch_user(self):
+        """ validate edit patch Users """
+        factory.defaults["HTTP_AUTHORIZATION"] = "Bearer " + self.token
+        response = factory.patch(
+            self.url + '4/',
+            data={
+                "username": "andresvz91",
+                "first_name": "andres",
+                "last_name": "vasquez"
+            },
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete_user(self):
+        """ validate delete User """
+
+        factory.defaults["HTTP_AUTHORIZATION"] = "Bearer " + self.token
+        response = factory.delete(
+            self.url + '4/',
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
